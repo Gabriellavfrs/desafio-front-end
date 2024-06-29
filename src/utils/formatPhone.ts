5156242bca3bf7101a17f5e17afdeacd44b952ea
Gabriella Vieira <gabvi2015@outlook.com>
2024-06-29 16:50:45 -0300

    fix phone formater

--- src/utils/formatPhone.ts
+++ src/utils/formatPhone.ts
@@ -1,4 +1,4 @@
 export const formatPhone = (phone: string) => {
   const regex = /^(\d{2})(\d{2})(\d{5})(\d{4})$/;
-  return phone.replace(regex, "+($1) $2 $3-$4");
+  return phone.replace(regex, "+$1 ($2) $3-$4");
 };
